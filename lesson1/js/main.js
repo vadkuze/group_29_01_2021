e5e5cd42feae8087ba0542889382c7233f9cc14b
Vadim Kuznietsov <vadkuze@gmail.com>
2021-01-29 22:16:47 +0200

    lesson 1

---
+++ lesson1/js/main.js
@@ -0,0 +1,57 @@
+console.log('Number');
+
+let n1 = 10;
+const n2 = 2;
+
+n1 = '19';
+
+let n3 = n1 + n2;
+
+console.log(n1, n2, n3);
+console.log( isNaN(10));
+console.log( isFinite(10));
+console.log('String');
+
+let string = "str";
+let string2 = 'str2';
+let string3 = `str3`;
+let age = 20;
+
+// alert(`Hi, I'm ${age} y.o.`)
+
+
+console.log('Boolean');
+// console.log(true || false)
+// console.log(true || true)
+// console.log(false || true)
+// console.log('&&');
+// console.log(true && false)
+// console.log(true && true)
+// console.log(false && true)
+
+
+// !-Infinity
+// true
+// !!0
+// false
+// !0
+// true
+// +'eeeee'
+// NaN
+// +'10'
+// 10
+// 20 + ''
+// "20"
+// Number('20')
+// 20
+// Boolean(0)
+// false
+
+console.log('' && 0 || 5 && null);
+
+let f;
+console.log(f);
+
+// -- 
+// ++
+// == ===    != !==
