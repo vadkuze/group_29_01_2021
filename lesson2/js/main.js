88dba3fca888dababac6b9d1c290427eddb3cf6a
Vadim Kuznietsov <vadkuze@gmail.com>
2021-01-31 19:42:46 +0200

    lesson 2

---
+++ lesson2/js/main.js
@@ -0,0 +1,97 @@
+// -- 
+// ++
+// == ===    != !==
+
+// let a = 10;
+// console.log(a--);
+// console.log(a);
+// console.log(--a);
+
+
+// let b = 10;
+// console.log(b++);
+// console.log(b);
+// console.log(++b);
+
+// a += 5; // a = a + 5
+
+
+// console.log( true != 1, '!='); 
+// console.log( true !== 1, '!==');
+
+// console.log( true == 1, '=='); 
+// console.log( true === 1, '===');
+
+
+// console.log( 'true' == "true", '=='); 
+// console.log( 'true' === 'tre', '===');
+
+// console.log( null == 0, '=='); 
+// console.log( +null == 0, '=='); 
+
+// console.log(NaN == NaN, '==');
+// console.log('' == 0, '==');
+// console.log('' === 0, '===');
+
+// const PI = 3.14;
+// const DELAY_MS = 1000;
+
+// let role = prompt('Enter your role');
+// console.log(typeof role);
+
+// if(role == null) {
+//     console.log('you canceled enter');
+// } else if(!role) {
+//     console.log('Enter something pls');
+// } else {
+
+//     if(role == 'admin') {
+//         console.log('You have full access for this page');
+//     } else if(role == 'manager') {
+//         console.log('You can edit articles');
+//     } else {
+//         console.log('Are u user ?')
+//     }
+// }
+
+// switch (role) {
+//     case 'admin': 
+//     case 'superadmin': {
+//         console.log('You have full access for this page');
+//         // ...
+//     }
+//     break;
+//     case 'manager': {
+//         console.log('You can edit articles');
+//     }
+//     break;
+
+//     default: {
+//         if (role == null) {
+//             console.log('you canceled enter');
+//         } else if (!role) {
+//             console.log('Enter something pls');
+//         }
+//     }
+// }
+
+// let status = 'initial';
+
+
+// status = !status ? 'start' : 'initial'; 
+
+// let result = role == 'admin' ? 'You have full access for this page' : 
+//              role == 'manager' ? 'You can edit articles' : 
+//              role == null ? 'U canceled' : 'Enter something pls';
+
+// console.log(result);
+// let coffeeSize = '';
+// let  result;
+
+// if(coffeeSize == null) {
+//     result = 'you canceled'
+// } else {
+//     result = `you should pay for ${coffeeSize == 1 ? 15 : coffeeSize == 2 ? 30 : 45} uah`
+// }
+// console.log(result)
+
